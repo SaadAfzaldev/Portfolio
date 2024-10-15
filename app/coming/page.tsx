b0e909bc34073f210bc53c0cd29ddb1b4fc190f0
saad <saaddevtec@gmail.com>
2024-10-15 18:30:09 +0530

    launch added

--- app/coming/page.tsx
+++ app/coming/page.tsx
@@ -6,7 +6,7 @@ export default function page() {
   return (
         <div className='flex justify-center h-screen bg-black-100'>
             <h1 className='flex justify-center items-center text-6xl'>
-                <TextGenerateEffect words='Coming Soon'/>
+                <TextGenerateEffect words='Launching Soon'/>
             </h1>
         </div>
   )
