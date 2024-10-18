2c472f7a31af64ecc96e577c2331f7ac7dea1728
Saad <saaddevtec@gmail.com>
2024-10-18 12:39:02 +0530

    mail solved

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
             Reach out to me today and let&apos;s discuss how I can help you
             achieve your goals.
         </p>
-        <a href="mailto:contactsaaddevtec@gmail.com">
+        <a href="mailto:saaddevtec@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
